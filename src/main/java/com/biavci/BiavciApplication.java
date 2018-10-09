d45bc17d5ea0879aeaafa7840715fa1dbc9912db
burakdmn <burakduman1892@gmail.com>
2018-10-09 13:58:28 +0300

    Initial commit

---
+++ src/main/java/com/biavci/BiavciApplication.java
@@ -0,0 +1,12 @@
+package com.biavci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiavciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiavciApplication.class, args);
+	}
+}
